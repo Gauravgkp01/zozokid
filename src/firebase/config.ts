3bbf5cbb9a05310ca330057b5e7848867fa17b1d
Garima Paswan <Garimapaswan200410@gmail.com>
2026-02-15 17:10:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9158004326-e80e7",
+  "appId": "1:811057287056:web:39088c1624e3cd0723ed15",
+  "apiKey": "AIzaSyAsA286ZWgdrWmG4vB6QgVzbs-3jj3fWVI",
+  "authDomain": "studio-9158004326-e80e7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "811057287056"
+};
